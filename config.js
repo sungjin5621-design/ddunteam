c4b0908b051841258ace35e2fdb7cd645342ddcb
sungjin5621-design <sungjin5621@gmail.com>
2026-09-04 09:50:00 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Fill these two values from Supabase Project Settings -> API.
 window.DDUNTEAM_CONFIG = {
-  supabaseUrl: 'https://YOUR_PROJECT.supabase.co',
+  https://tspaizoqfexkkoolwoec.supabase.co,
   supabasePublishableKey: 'sb_publishable_U3sqJuX1GYGa7jzUIhfHBw_rmRZPMP6'
 };
